b270f30e26a341a510d17f8bef9b0f3424173cb8
monamoves <monamoves@gmail.com>
2018-09-24 12:33:41 -0400

    latest updates

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // thanks Fitorec
 $(document).ready(function(){
-  var animate_loop, colors, i;
   colors = ['#7f7f92', '#927f7f', '#92927f', '#929292'];
-  i = 0;
+  var i = 0;
   animate_loop = function() {
-     $("html body").animate({ backgroundColor: colors[i++ % colors.length] }, 4000, function() {
+     $("html body").animate({ backgroundColor: colors[i++%colors.length] }, 4000, function() {
       animate_loop();
                            });
-  };
+  }
   animate_loop();
 });
+
+
+
